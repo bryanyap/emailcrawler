3a0267eec2bcc57aadfefba8c2250e32d29d7e16
Bryan Yap <bryanyap2002@hotmail.com>
2016-05-13 02:06:16 +0800

    initial commit

---
+++ emailcrawler/spiders/emailspider.py
@@ -0,0 +1,18 @@
+from emailcrawler.items import EmailcrawlerItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Request
+from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+
+
+class EmailSpider(CrawlSpider):
+    name = "faircent"
+    allowed_domains = ["faircent.com"]
+    start_urls = ["https://www.faircent.com/"]
+    item = EmailcrawlerItem()
+
+    rules = (Rule(LxmlLinkExtractor(allow=()), callback='parse_obj', follow=True),)
+
+    def parse_obj(self, response):
+        for link in LxmlLinkExtractor(allow=(), deny=self.allowed_domains).extract_links(response):
+            item = EmailcrawlerItem()
+            item['url'] = link.url
