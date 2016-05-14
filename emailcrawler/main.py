03dbe01e474bb33eb6e33434b7c3c09b307810bf
Bryan Yap <bryanyap2002@hotmail.com>
2016-05-14 14:01:18 +0800

    added user input in main.py

--- emailcrawler/main.py
+++ emailcrawler/main.py
@@ -1,4 +1,5 @@
 import os
+from sys import argv
 
 import tldextract
 from scrapy.crawler import CrawlerProcess
@@ -6,9 +7,22 @@ from scrapy.utils.project import get_project_settings
 
 from spiders.emailspider import EmailSpider
 
+if len(argv) != 2:
+    raise ValueError('Please key in the correct number of inputs')
+
+try:
+    input_number = int(argv[1])
+except:
+    raise ValueError('Please key in an integer value')
+
 try:
     os.remove('items.json')
 except OSError:
+    pass
+
+try:
+    os.remove('scrapedata.db')
+except:
     pass
 
 process = CrawlerProcess(get_project_settings())
@@ -16,8 +30,9 @@ counter = 0
 
 with open('/home/bryan/Desktop/tia_selected_websites.csv') as f:
     for line in f:
-        if counter == 50:
-            break
+        if counter == 0:
+            if counter == input_number:
+                break
         start_url = line.split('\t')[1]
         name = line.split('\t')[0]
 
--- emailcrawler/pipelines.py
+++ emailcrawler/pipelines.py
@@ -6,7 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import json
-import os
 import sqlite3
 
 from scrapy import log
@@ -16,27 +15,23 @@ class EmailcrawlerPipeline(object):
     def __init__(self):
         self.file = open('items.json', 'a')
 
-        try:
-            os.remove('scrapedata.db')
-        except:
-            pass
-
         self.connection = sqlite3.connect('scrapedata.db')
         self.cursor = self.connection.cursor()
         self.cursor.execute('CREATE TABLE IF NOT EXISTS email (address VARCHAR(100), PRIMARY KEY(address))')
         self.cursor.execute('CREATE TABLE IF NOT EXISTS scrape (domain VARCHAR(100), address VARCHAR(100), url VARCHAR(500), PRIMARY KEY(domain, address), FOREIGN KEY(address) REFERENCES emails(address))')
+        self.connection.commit()
 
     def process_item(self, item, spider):
         line = json.dumps(dict(item)) + '\n'
         self.file.write(line)
 
-        emails = item['emails']
-        for email in emails:
+        for email in item.emails:
             self.cursor.execute("select * from email where address=?", email)
             result = self.cursor.fetchone()
             if not result:
                 self.cursor.execute("insert into email (address) values (?)", email)
-                self.cursor.execute("insert into scrape (domain, address, url) values (?,?,?)", item['domain'], email, item['url'])
+                self.cursor.execute("insert into scrape (domain, address, url) values (?,?,?)", item.domain, email,
+                                    item.url)
                 self.connection.commit()
                 log.msg("Item stored : " % email, level=log.DEBUG)
             else:
